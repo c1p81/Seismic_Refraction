3fb5698cc2022cf6f5c5581b3134aa0f0c95654f
c1p81 <lucainnoc@gmail.com>
2020-04-13 14:42:20 +0200

    UDP Client

--- app/src/main/java/com/luca/innocenti/seismicrefraction/MainActivity.kt
+++ app/src/main/java/com/luca/innocenti/seismicrefraction/MainActivity.kt
@@ -5,22 +5,32 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
 import android.os.Environment
+import android.os.StrictMode
 import android.util.Log
-import android.widget.TextView
-import android.widget.Toast
-import android.widget.ToggleButton
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import com.lyft.kronos.AndroidClockFactory
 import com.lyft.kronos.KronosClock
 import com.lyft.kronos.SyncListener
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 import java.io.FileOutputStream
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.InetAddress
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import kotlin.math.sqrt
+
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
+    private lateinit var socket: DatagramSocket
+    private var formatter: DateTimeFormatter? = null
+    private var formatted: String? = null
     private lateinit var fileOutPutStream: FileOutputStream
     private var msg: String = "OFF"
     private var mAccelerometer: Sensor? = null
@@ -29,10 +39,14 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private var tempo: Long = 0
     private var conteggio: Long = 0
+    private var nr_stazione: Int = 1
     lateinit var kronosClock: KronosClock
 
-    private val filepath = "MyFileStorage"
+
+    private val filepath = "Seismic_Refraction"
     internal var myExternalFile: File?=null
+    
+
     private val isExternalStorageReadOnly: Boolean get() {
         val extStorageState = Environment.getExternalStorageState()
         return if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(extStorageState)) {
@@ -54,30 +68,71 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        myExternalFile = File(getExternalFilesDir(filepath), "dati.csv")
-
-        if (!isExternalStorageAvailable || isExternalStorageReadOnly) {
-            Log.d("Errore", "non posso scrivere")
-        }
-
-        fileOutPutStream = FileOutputStream(myExternalFile)
-
-
-
         mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        mAccelerometer = mSensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        mAccelerometer = mSensorManager!!.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)
+
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
 
 
 
         val testo = findViewById<TextView>(R.id.testo)
+        val iptxt = findViewById<EditText>(R.id.iptxt)
+        val stazione = findViewById<TextView>(R.id.stazione)
+        stazione.setText("Station nr: 1")
+
         val campioni = findViewById<TextView>(R.id.campioni)
+        val barra = findViewById<SeekBar>(R.id.seekBar)
+
+        seekBar?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                // Write code to perform some action when progress is changed.
+                //Toast.makeText(this@MainActivity, "Progress is " + seekBar.progress + "%", Toast.LENGTH_SHORT).show()
+
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // Write code to perform some action when touch is started.
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // Write code to perform some action when touch is stopped.
+                nr_stazione = seekBar.progress + 1
+                stazione.setText("Station Nr: "+ nr_stazione.toString())
+                Log.d("file", myExternalFile.toString())
+            }
+        })
+
 
         val toggleButton = findViewById<ToggleButton>(R.id.toggleButton)
         toggleButton?.setOnCheckedChangeListener { buttonView, isChecked ->
-              if (isChecked) msg="ON" else msg="OFF"
-            Toast.makeText(this@MainActivity, "Acquisition is "+msg, Toast.LENGTH_SHORT).show()
-            conteggio = 0
+            if (isChecked) {
+                    msg = "ON"
+                    socket = DatagramSocket()
+                    socket.broadcast = false
+
+
+                    conteggio = 0
+                    val current = LocalDateTime.now()
+                    formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH:mm:ss")
+                    formatted = current.format(formatter)
+
+                    myExternalFile = File(
+                        getExternalFilesDir(filepath),
+                        formatted + "_" + nr_stazione.toString() + ".csv"
+                    )
+                    fileOutPutStream = FileOutputStream(myExternalFile)
+                    Toast.makeText(this@MainActivity, "Acquisition is " + msg, Toast.LENGTH_SHORT).show()
         }
+            else{
+                msg="OFF"
+                socket.close()
+                fileOutPutStream.close()
+                Toast.makeText(this@MainActivity, "File saved : " + formatted+".csv" , Toast.LENGTH_LONG).show()
+            }
+
+        }
+
 
         val syncListener: SyncListener = object : SyncListener {
             override fun onStartSync(host: String) {
@@ -110,20 +165,27 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     override fun onSensorChanged(event: SensorEvent?) {
         if (event != null) {
-            if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            if (event.sensor.type == Sensor.TYPE_LINEAR_ACCELERATION) {
                 var x = event.values[0]
                 var y = event.values[1]
                 var z = event.values[2]
+                var m = sqrt((x*x)+(y*y)+(z*z))
+
 
 
 
                 if (msg == "ON") {
                     conteggio = conteggio + 1
-                    campioni.setText(conteggio.toString())
+                    campioni.setText("Samples Nr : " + conteggio.toString())
                     tempo = kronosClock.getCurrentTimeMs()
-                    var salva: String = tempo.toString() + ";" + x.toString() + ";" + y.toString() + ";" + z.toString()+"\n"
+                    //var salva: String = nr_stazione.toString()+";"+tempo.toString() + ";" + x.toString() + ";" + y.toString() + ";" + z.toString()+";"+m.toString()+"\n"
+                    var salva: String = nr_stazione.toString()+";"+tempo.toString() + z.toString()+";"+m.toString()+"\n"
 
                     fileOutPutStream.write(salva.toByteArray())
+
+                    val byte = salva.toByteArray();
+                    val packet = DatagramPacket(byte, byte.size, InetAddress.getByName(iptxt.text.toString()),5550);
+                    socket.send(packet);
 
                     Log.d(
                             "Seismo",conteggio.toString()+";"+
